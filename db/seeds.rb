108c9b92fb84a29e00a72cf8ad5ba3702c12d448
Will <creaked@gmail.com>
2020-08-15 17:23:48 +0000

    add user and message model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User.create(username: "Jon", password: "password")
+# User.create(username: "Steve", password: "password")
+# User.create(username: "Mike", password: "password")
+# User.create(username: "Chris", password: "password")
+# User.create(username: "Paul", password: "password")
+
+Message.create(body: "Hi, I'm a new message as well", user: User.first)
